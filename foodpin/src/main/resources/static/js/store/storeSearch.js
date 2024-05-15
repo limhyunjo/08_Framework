0ae26b69260131480acd86a6ad4b12a4c2a15db4
limhyunjo <sibagyeoni@gmail.com>
2024-05-15 18:44:36 +0900

    final- FoodPin 지도api

--- foodpin/src/main/resources/static/js/store/storeSearch.js
+++ foodpin/src/main/resources/static/js/store/storeSearch.js
@@ -1,3 +1,5 @@
+
+function storelocation(){
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
@@ -16,7 +18,7 @@ if (navigator.geolocation) {
             lon = position.coords.longitude; // 경도
         
         var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-            message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+            message = '<div style="padding:5px;">현재 위치</div>'; // 인포윈도우에 표시될 내용입니다
         
         // 마커와 인포윈도우를 표시합니다
         displayMarker(locPosition, message);
@@ -34,7 +36,10 @@ if (navigator.geolocation) {
 // 지도에 마커와 인포윈도우를 표시하는 함수입니다
 function displayMarker(locPosition, message) {
 
-    // 마커를 생성합니다
+
+    /* 나중에 현재 위치는 마커 지우고 근처 가게들에 마커 생성 */
+
+    // 마커를 생성합니다  
     var marker = new kakao.maps.Marker({  
         map: map, 
         position: locPosition
@@ -55,3 +60,10 @@ function displayMarker(locPosition, message) {
     // 지도 중심좌표를 접속위치로 변경합니다
     map.setCenter(locPosition);      
 }    
+}
+
+/* 화면 생성 시 자동 실행 */
+window.onload = function() {
+    // currentlocation 함수 실행
+    storelocation();
+  };

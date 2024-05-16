5e2dac1f9a3a45916cbdf9fb9eca8ac8db8f38a9
limhyunjo <sibagyeoni@gmail.com>
2024-05-16 09:10:20 +0900

    foodpin 화면 구현

--- foodpin/src/main/resources/static/js/store/storeDetail.js
+++ foodpin/src/main/resources/static/js/store/storeDetail.js
@@ -7,22 +7,3 @@ storedetailmapbutton.addEventListener("click", () => {
 });
 
 
-/* 폐점/ 정보 정정 신고 팝업 */
-var target = document.querySelectorAll('.btn_open');
-var btnPopClose = document.querySelectorAll('.pop_wrap .btn_close');
-var targetID;
-
-// 팝업 열기
-for(var i = 0; i < target.length; i++){
-  target[i].addEventListener('click', function(){
-    targetID = this.getAttribute('href');
-    document.querySelector(targetID).style.display = 'block';
-  });
-}
-
-// 팝업 닫기
-for(var j = 0; j < target.length; j++){
-  btnPopClose[j].addEventListener('click', function(){
-    this.parentNode.parentNode.style.display = 'none';
-  });
-}

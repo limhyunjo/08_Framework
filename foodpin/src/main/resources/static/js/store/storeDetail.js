525eccda78e7ab9225d6c5550ce3c251a438a84f
limhyunjo <sibagyeoni@gmail.com>
2024-05-23 21:25:49 +0900

    foodpin 파이널에서 가져옴

--- foodpin/src/main/resources/static/js/store/storeDetail.js
+++ foodpin/src/main/resources/static/js/store/storeDetail.js
@@ -204,7 +204,6 @@ shutMenuImageBtn.addEventListener("click",()=>{
   moreMenuImageBtn.style.display = 'inline';
 
   storeMenuList.style.display = 'flex';
- /*  storeMenuList.style.height = '370px'; */
 
   storeMenuList.style.overflow = 'hidden';
 
